64b200bf49f4258f3ce08af8fc94bdec5e3c71e7
Nigel Delaney <spamavoid@outlook.com>
2016-05-30 13:00:08 -0700

    Update Library Name

--- src/monovc/build.sh
+++ src/monovc/build.sh
@@ -29,7 +29,7 @@ mkbundle -c -o mono_embed_host.c -oo mono_embed_libs.o --static --keeptemp --dep
 clang -g -c -Wall `pkg-config --cflags mono-2` -o temp_hold.o mono_embed_libs.o mono_embed_host.c
 ld -r temp_hold.o mono_embed_libs.o -o embeded_libs.o
 # the C++ bits
-clang++ -dynamiclib -o variantcaller.so -D PROGRAM -I../include -framework CoreFoundation -lobjc -liconv -Wall `pkg-config --cflags mono-2`  `pkg-config --libs-only-L mono-2` `pkg-config --variable=libdir mono-2`/libmono-2.0.a `pkg-config --libs-only-l mono-2 | sed -e "s/\-lmono-2.0 //"` embeded_libs.o ../VariantCaller.cpp
+clang++ -dynamiclib -o libVariantCaller.so -D PROGRAM -I../include -framework CoreFoundation -lobjc -liconv -Wall `pkg-config --cflags mono-2`  `pkg-config --libs-only-L mono-2` `pkg-config --variable=libdir mono-2`/libmono-2.0.a `pkg-config --libs-only-l mono-2 | sed -e "s/\-lmono-2.0 //"` embeded_libs.o ../VariantCaller.cpp
 # NOW EXECUTABLE
 clang++ -g -o variantcaller -D PROGRAM -I../include -I../ -framework CoreFoundation -lobjc -liconv -Wall `pkg-config --cflags mono-2`  `pkg-config --libs-only-L mono-2` `pkg-config --variable=libdir mono-2`/libmono-2.0.a `pkg-config --libs-only-l mono-2 | sed -e "s/\-lmono-2.0 //"` embeded_libs.o ../VariantCaller.cpp 
 # Clean and test
